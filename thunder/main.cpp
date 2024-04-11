0434ec19bd52836b1e8d52aa1cd4bfd482ec8329
OrKeren8 <35062001+OrKeren8@users.noreply.github.com>
2024-04-11 17:23:10 +0300

    Revert "First merge"

--- thunder/main.cpp
+++
@@ -1,8 +0,0 @@
-#include <iostream>
-
-int main() {
-    std::cout << "Hello, World!" << std::endl;
-    std::cout << "testing" << std::endl;
-    std::cout << "first branch" << std::endl;
-    return 0;
-}
