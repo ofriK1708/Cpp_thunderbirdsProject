88e93a08be83c62fd99665a8d6f3112781264f0d
or keren <orkeren8@gmail.com>
2024-04-24 23:11:28 +0300

    add some docstrings

--- thunder/ship.cpp
+++ thunder/ship.cpp
@@ -9,6 +9,12 @@ void Ship::init(char symbol, int color)
 	this->backgroundcolor = color;
 }
 
+
+/**
+ * Moves the ship in the specified direction.
+ *
+ * @param direction The direction in which to move the ship.
+ */
 void Ship::move(GameConfig::eKeys direction)
 {
 	Point lastPos[MAX_SHIP_SIZE];
@@ -22,6 +28,12 @@ void Ship::move(GameConfig::eKeys direction)
 	delTrace(lastPos);
 }
 
+
+/**
+ * Deletes the trace of the ship.
+ *
+ * @param lastPos An array of Points representing the previous positions of the ship.
+ */
 void Ship::delTrace(Point lastPos[]) {
 	bool toDel;
 	for (int i = 0; i < size; i++) {
